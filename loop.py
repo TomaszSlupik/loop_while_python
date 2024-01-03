3986f814634b3923e8ce189f0b28e4e4d42f754d
TomaszSlupik <slupiktomasz@gmail.com>
2024-01-03 17:54:13 +0100

    loop

--- loop.py
+++ loop.py
@@ -19,3 +19,43 @@ while len(ten_number) < 10:
     number += 1
 
 print(ten_number)
+
+print('---')
+
+
+# Napisz program, który przy użyciu pętli while oblicza silnię z danej liczby całkowitej
+
+def countStrong (num):
+    yourNum = []
+    number = 0
+    while number < num:
+        number += 1
+        yourNum.append(number)
+
+    multipliedNumbers = 1 
+    
+    for finalStrong in yourNum:
+        multipliedNumbers  *= finalStrong
+    print (f"The factorial is: {multipliedNumbers}")
+
+countStrong(5)
+
+print('---')
+
+# Napisz program który wyświetla kolejne elementy ciągu Fibonacciego aż do momentu przekroczenia zadanej wartości np.100
+yourLimit = 100
+
+def fibonacci(limit):
+    fibonacciList = [0, 1]
+    while True:
+        nextFibonacci = fibonacciList[-1] + fibonacciList[-2]
+        if nextFibonacci >= limit:
+            break
+        fibonacciList.append(nextFibonacci)
+    
+    for num in fibonacciList:
+        print(num)
+
+fibonacci(yourLimit)
+
+
